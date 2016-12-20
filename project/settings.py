a334f87119649c8da830ac27ee29a0acd30be039
jhongyi <a0922705975@gmail.com>
2016-12-20 17:49:19 +0800

    add module (inventory, sales, stock)

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # module
+    'stocks',
+    'inventory',
+    'sales',
+
+    # third-party
     'rest_framework',
     'rest_framework_docs',
 ]
